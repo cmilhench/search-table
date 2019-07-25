e3d71c03006af02e654dea7039374baed3bcf8f3
cmilhench <cmilhench@gmail.com>
2019-07-25 16:41:07 +0100

    Initial project load

---
+++ search-table/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  search-table
+//
+//  Created by Colin Milhench on 25/07/2019.
+//  Copyright © 2019 Colin Milhench. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
